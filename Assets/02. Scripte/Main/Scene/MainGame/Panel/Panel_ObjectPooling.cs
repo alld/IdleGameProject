69136d736013b43d5df4230c147cce80229cc841
JOJAEHOON\USER <tjk851225@naver.com>
2024-06-05 17:13:00 +0900

    [0.002] feat : Dictionary 오브젝트 풀링 테스트 중

--- Assets/02. Scripte/Main/Scene/MainGame/Panel/Panel_ObjectPooling.cs
+++ Assets/02. Scripte/Main/Scene/MainGame/Panel/Panel_ObjectPooling.cs
@@ -20,25 +20,46 @@ public class Panel_ObjectPooling : MonoBehaviour
 
     private List<GameObject> poolList;           // 오브젝트 풀링 리스트
 
+    public enum EnemyType
+    {
+        None = 0,
+        Orc,
+        Zombie,
+        Skeleton,
+        Slime,
+        Goblin          // 기타 등등... (임시 명칭)
+    }
+
+    private Dictionary<EnemyType, List<GameObject>> poolDic;
+
     private void Start()
     {
         poolList = new List<GameObject>();       // 초기화
 
         for(int i = 0; i < poolSize; i++)
         {
-            //GameObject targetObject = Instantiate(enemyPool);   // 우선 한개만 넣고 3가지 다 넣을떄는 함수로 넣자
+            //실전용
+            //GameObject targetObject = Instantiate(enemyPool);     // 우선 한개만 넣고 3가지 다 넣을떄는 함수로 넣자
             //targetObject.SetActive(false);                        // 비활성화
             //poolList.Add(targetObject);                           // 리스트에 추가
-            CreateObject();                                     // 오브젝트 생성 (임시)
+
+            //딕셔너리 테스트용
+            DictionaryCreateObject();                               // 딕셔너리 생성 (임시)
+
+            //테스트용
+            //CreateObject();                                       // 오브젝트 생성 (임시)
         }
     }
 
+    /// <summary>
+    /// 오브젝트 생성 및 초기 자리를 세팅합니다.
+    /// </summary>
     private void CreateObject()
     {
         GameObject createObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
+        // 생성 설정
         createObj.transform.SetParent(gameObject.transform);
-        // 생성 위치 설정 (범위)
         createObj.name = "Enemy";
         createObj.transform.position = new Vector3(Screen.width + Random.Range(Screen.width * 0.05f, Screen.width * 0.1f), 
                                                     Screen.height / 2f + Random.Range((Screen.height / 2f) * -0.2f, (Screen.height / 2f) * 0.2f));   // 화면 범위 밖에서 생성 (매직 넘버는 개선 필요)
@@ -46,7 +67,42 @@ public class Panel_ObjectPooling : MonoBehaviour
         createObj.transform.localScale = new Vector3(20f, 20f, 1f);
     }
 
-    // 오브젝트 가지고 오기
+    /// <summary>
+    /// 딕셔너리용 오브젝트 생성 및 초기자리 세팅
+    /// </summary>
+    private void DictionaryCreateObject()
+    {
+        ObjectPoolingSetting();    
+
+        GameObject createDicObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        createDicObject.name = "Enemy";
+        createDicObject.tag = "Enemy";
+        createDicObject.transform.SetParent(gameObject.transform);
+        createDicObject.transform.position = new Vector3(Screen.width + Random.Range(Screen.width * 0.05f, Screen.width * 0.1f),
+                                                    Screen.height / 2f + Random.Range((Screen.height / 2f) * -0.2f, (Screen.height / 2f) * 0.2f));   // 화면 범위 밖에서 생성 (매직 넘버는 개선 필요)
+        createDicObject.transform.rotation = Quaternion.identity;
+        createDicObject.transform.localScale = new Vector3(20f, 20f, 1f);
+
+
+        poolDic.Add(EnemyType.Orc, new List<GameObject>());
+    }
+
+    /// <summary>
+    /// 생성할 오브젝트 세팅
+    /// </summary>
+    private void ObjectPoolingSetting()
+    {
+        poolDic.Add(EnemyType.Orc, new List<GameObject>());
+        poolDic.Add(EnemyType.Zombie, new List<GameObject>());
+        poolDic.Add(EnemyType.Skeleton, new List<GameObject>());
+        poolDic.Add(EnemyType.Slime, new List<GameObject>());
+        poolDic.Add(EnemyType.Goblin, new List<GameObject>());
+    }
+
+    /// <summary>
+    /// 오브젝트 가지고 오기 
+    /// </summary>
     private GameObject GetObject()
     {
         foreach(GameObject obj in poolList)
